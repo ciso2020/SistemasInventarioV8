03137a65506fa6273a6c64029587607792dcff4e
ciso2020 <cisoivan2020@gmail.com>
2025-02-11 01:25:37 -0600

    Todos los proyectos Net8

---
+++ copia/SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SistemaInventario.Modelos;
+
+namespace SistemaInventario.AccesoDatos.Data
+
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(Microsoft.EntityFrameworkCore.DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Bodega> Bodegas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            base.OnModelCreating(builder);
+
+        }
+    }
+
+    
+}
---
+++ copia/SistemaInventario/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace SistemaInventario.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(Microsoft.EntityFrameworkCore.DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
